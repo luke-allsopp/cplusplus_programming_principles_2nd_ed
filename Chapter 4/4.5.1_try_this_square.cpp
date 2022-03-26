830872e4a6a17c806cbc9dae8b8922bbdc08c79d
allsolu <lukeallsopp93@gmail.com>
2022-03-26 22:24:07 +1100

    ch 4.5

---
+++ Chapter 4/4.5.1_try_this_square.cpp
@@ -0,0 +1,25 @@
+//4.5.1 try this square
+
+#include"../std_lib_facilities.h"
+
+int square(int x)
+{
+    int s {0};
+    for(int i=0; i<x; ++i)
+    {
+        s+=x;
+    }
+
+    return s;
+}
+
+
+int main ()
+{
+    cout << "Number:" << '\t' << "Square:" << '\n';
+
+    for (int i=0; i<100; ++i)
+        cout << i << '\t' << square(i) << '\n';
+
+    return 0;
+}
