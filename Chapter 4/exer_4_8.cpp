fe38d9b58a2645438a60e9622dcae1103ae3ecc5
allsolu <lukeallsopp93@gmail.com>
2022-06-03 09:07:29 +1000

    fixed

--- Chapter 4/exer_4_8.cpp
+++ Chapter 4/exer_4_8.cpp
@@ -3,34 +3,21 @@
 /*emperor wants to thank the inventor of chess, gets 1 wish:
 the inventor asked for 1 grain of rice for the 1st chess square, 2 for the 2nd, 4 for the 3 and so on
 doubling for each for the 64 squares
-calcluate how many squares needed to give:
-- 1000 rice grains
-- 1000000 grains
-- 1000000000 grains 
-
-
-in prog - giving shit results
-*/
-
-int square(double v)
-{
-    return v*v;
-}
-
+calcluate how many squares needed to give rice */
 int main()
 {
-    double sum, x = 0;
-    int prize1 = 1000;
-    int prize2 = 1000000;
-    int prize3 = 1000000000;
-
-    cout << "square #:" << "  " << "rice for this square:" << "total rice:\n";
-
-    for (int i = 1; i < 65; i++)
-    {         
-        sum += x = square(i);
-        cout << i << "      " << x << "     " <<sum <<'\n';
-    }
-
-    return 0;    
+	int square = 1;
+	int rice = 1;
+	int target = 0;
+	   
+	cout << "How many grains do you want?\n";
+	cin >> target;
+	    
+	while (rice < target) 
+    {
+	  	rice *= 2;
+        square += 1;
+	}
+	    
+        cout << "# of grains of rice: " << rice << " chess square #: " << square << '\n';
 }
